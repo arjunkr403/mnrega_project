fbcadf8418b53977a9718fd70636a83db576eef8
arjunkr403 <arjunkr403@gmail.com>
2025-10-30 19:21:51 +0530

    Add server proxy configuration for API requests in Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api/mgnrega': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
